4ed29c248404245e2a81d7c238b54b4092cda840
StefanoBelli <stefano9913@gmail.com>
2024-04-17 07:03:19 +0200

    change

--- project/src/main/java/ste/model/Release.java
+++ project/src/main/java/ste/model/Release.java
@@ -6,12 +6,14 @@ import ste.Util;
 import ste.jirarest.JiraProject;
 
 public final class Release {
+    private final String id;
     private final String version;
     private final Date releaseDate;
     
-    private Release(String version, Date releaseDate) {
+    private Release(String id, String version, Date releaseDate) {
         this.version = version;
         this.releaseDate = releaseDate;
+        this.id = id;
     }
 
     public Date getReleaseDate() {
@@ -22,9 +24,14 @@ public final class Release {
         return version;
     }
 
+    public String getId() {
+        return id;
+    }
+
     public static Release fromJiraVersion(JiraProject.Version version) {
         String rd = version.getReleaseDate();
         return new Release(
+            version.getId(),
             version.getName(), 
             rd != null ? Util.dateFromString(rd) : null);
     }
