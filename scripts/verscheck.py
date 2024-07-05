02ef8a70ef35c87bf4cd360eb40be017aa15e01c
StefanoBelli <stefano9913@gmail.com>
2024-07-05 22:20:16 +0200

    update scripts/verscheck.py

--- scripts/verscheck.py
+++ scripts/verscheck.py
@@ -21,23 +21,39 @@ if __name__ == '__main__':
         print("missing version file path")
         exit(1)
 
+    totalln = 0
+
     with open(argv[1]) as versions_f:
         reader = csv.reader(versions_f)
         oldfv = maxsize
         ln = 1
+        skipped_asc_should_break = False
         for row in reader:
+            if row[0] == "...":
+                skipped_asc_should_break = True
+                ln += 1
+                continue
+
             comps = cvtstr2int(row)
             if oldfv > comps[2]:
-                print(f" --> line {ln} breaks ascending order")
+                if skipped_asc_should_break:
+                    print(f" --> line {ln} breaks ascending order (expected={skipped_asc_should_break})")
+                    skipped_asc_should_break = False
+                else:
+                    print(f" --> unexpected ascending order break - terminating now, line {ln}")
+                    exit(1)
             
             oldfv = comps[2]
             print(f"iv = {comps[0]}, ov = {comps[1]}, fv = {comps[2]}", end="")
             if rowrule(comps) == False:
-                print(" <-- not ok")
+                print(f" <-- not ok - terminating now, line {ln}")
                 exit(1)
             else:
-                print(" ok")
+                print(" OK")
             
             ln += 1
+        
+        totalln = ln - 1
 
+    print(f"graceful termination, line processed: {totalln}")
             
